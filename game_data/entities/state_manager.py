665a2ab87d3ab5c09f54537c07a6e2cfb81aa05b
kij1n <kij1n@proton.me>
2025-12-26 09:47:44 +0100

    backup before adding debug boxes

--- game_data/entities/state_manager.py
+++ game_data/entities/state_manager.py
@@ -5,10 +5,14 @@ class StateManager:
 
     def get_where(self, player_pos : tuple[int, int] = None) -> tuple[tuple[int,int], str]:
         if player_pos is None:
+            # Player: center of screen, offset by half sprite size
+            sprite_name = self.entity.get_sprite_name(self.state, 0)
+            hitbox_x = self.entity.settings["player_info"]["hitbox_x"]
+            hitbox_y = self.entity.settings["player_info"]["hitbox_y"]
             return (
                 self.entity.settings["screen"]['size_x'] // 2,
                 self.entity.settings["screen"]['size_y'] // 2,
-            ), self.entity.get_sprite_name(self.state, 0)
+            ), sprite_name
         else:
             return (
                 self.entity.get_position(),
