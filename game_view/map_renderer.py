2b1ab82070c91299738cd62ca34347e5d08f7bab
solnypiotr <alt.pit111k@gmail.com>
2026-01-09 11:32:42 +0100

    moved view classes to its own modules

---
+++ game_view/map_renderer.py
@@ -0,0 +1,42 @@
+import pytmx
+import pyscroll
+import os
+
+class MapRenderer:
+    def __init__(self, size, settings: dict):
+        self.map = MapLoader(size, settings)
+
+    def render(self, target, screen):
+        self.map.draw(target, screen)
+
+
+class MapLoader:
+    def __init__(self, size: tuple[int, int], settings: dict):
+        self.settings = settings
+
+        script_dir = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+        path = os.path.join(script_dir, settings["map"]["map_path"])
+
+        if os.name == "nt":
+            path = path.replace("/", "\\")
+
+        tmx_data = pytmx.load_pygame(path)
+        map_data = pyscroll.data.TiledMapData(tmx_data)
+
+        self.map_layer = pyscroll.BufferedRenderer(map_data, size)
+        self.group = pyscroll.PyscrollGroup(map_layer=self.map_layer)
+
+    def set_center(self, target):
+        target = (
+            self.settings["screen"]['size_x'] // 2
+            if target[0] < self.settings["screen"]['size_x'] // 2
+            else target[0],
+            self.settings["screen"]['size_y'] // 2
+            if target[1] < self.settings["screen"]['size_y'] // 2
+            else target[1]
+        )
+        self.group.center(target)
+
+    def draw(self, target, surface):
+        self.set_center(target)
+        self.group.draw(surface)
---
+++ game_view/sprite_loader.py
@@ -0,0 +1,64 @@
+import pygame
+import os
+
+class SpriteLoader:
+    def __init__(self, settings: dict):
+        self.sprites = (
+                self.load_player(settings) |
+                self.load_enemies(settings)
+        )
+
+    def load_player(self, settings: dict):
+        player = {}
+        for sprite_type in settings["player_info"]["sprites_paths"].keys():
+            index = 1
+            for sprite_location in settings["player_info"]["sprites_paths"][sprite_type]:
+                path = self._get_path(sprite_location)
+
+                image = pygame.image.load(path).convert_alpha()
+                sprite = Sprite(image)
+
+                if sprite_type == "arm":
+                    sprite_name = "player_" + sprite_type
+                elif sprite_type in ['guns', 'bullet']:
+                    delimiter = '\\' if os.name == "nt" else '/'
+                    sprite_name = path.split('.')[0].split(delimiter)[-1]
+                else:
+                    sprite_name = "player_" + sprite_type + str(index)
+                    index += 1
+
+                player[sprite_name] = sprite
+        return player
+
+    def load_enemies(self, settings: dict):
+        return {}
+
+    def get_sprite(self, sprite_name):
+        return self.sprites[sprite_name]
+
+    @staticmethod
+    def _get_path(sprite_location):
+        # create an absolute path to the sprite
+        script_dir = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+        path = os.path.join(script_dir, sprite_location)
+
+        if os.name == "nt":
+            path = path.replace("/", "\\")
+
+        return path
+
+
+class Sprite(pygame.sprite.Sprite):
+    def __init__(self, image: pygame.Surface):
+        super().__init__()
+        self.image = image
+        self.rect = self.image.get_rect()
+
+    def set_position(self, position):
+        self.rect.center = position
+
+    def invert_copy(self):
+        return pygame.transform.flip(self.image, True, False)
+
+    def rotate_copy(self, degrees):
+        return pygame.transform.rotate(self.image, degrees)
